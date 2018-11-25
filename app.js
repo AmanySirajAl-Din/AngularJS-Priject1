db771e994fc65058b9038f3cd4be33d8693ee2cd
Amany Siraj <amanysirajaldin@gmail.com>
2018-11-25 15:18:20 +0200

    Prestup app.js

--- app.js
+++ app.js
@@ -1,23 +1,4 @@
-// function that immediately invoked
-// to make sure that no local variables bleed into the global scope
 (function(){
-    // first, define our main app
-    // the thing that's going to be responsible for
-    // chunck of HTML in index.html
-    // angular.module("the name of the App", [dependencies needed] )
-    // we don't need dependencies so we will leave it as empty array
-    angular.module('myFirstApp', [])
-    
-    // controller is to define the viewModel of our view
-    // .controller('name of the controller, the functionality of it);
-    // $anything in angular it means that 'anything' is reserved for angular
-    // $scope an object that allows us to share data btw the viewModel and the view
-    //scope will be accesable in the controller component
-    .controller('myFirstController', function($scope){
-        $scope.name = "Amany";
-        $scope.sayHello = function(){
-            return "Hello Amany!";
-        };
-    });
+    'use strict';
      
 })();
